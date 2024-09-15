54612600e894cff492e53570e3c2f8bedc4a1dd7
Serhii Ternovtsov <tserg42@gmail.com>
2024-09-16 02:28:36 +0300

    combine character classes

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,53 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        input_line.contains(pattern)
-    } else if pattern == "\\d" {
-        input_line.chars().any(|c| matches!(c, '0'..'9'))
-    } else if pattern == "\\w" {
-        input_line
-            .chars()
-            .all(|c| matches!(c, 'a'..'z') || matches!(c, 'A'..'Z') || c == '_')
-    } else if pattern.len() > 2 && pattern.starts_with('[') && pattern.ends_with(']') {
-        if pattern.starts_with("[^") {
-            let negative_group = &pattern[2..pattern.len() - 1];
-            input_line.chars().all(|c| !negative_group.contains(c))      
-        } else  {
-            let positive_group = &pattern[1..pattern.len() - 1];
-            input_line.chars().any(|c| positive_group.contains(c))
+    let mut pattern_index = 0;
+    for c in input_line.chars() {
+        if pattern_index >= pattern.len() {
+            return true;
         }
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+        let remaining_pattern = &pattern[pattern_index..];
+        if remaining_pattern.starts_with("\\d") {
+            if matches!(c, '0'..'9') {
+                pattern_index += 2;
+            } else {
+                pattern_index = 0;
+            }
+        } else if remaining_pattern.starts_with("\\w") {
+            if matches!(c, 'a'..'z') || matches!(c, 'A'..'Z') || c == '_' {
+                pattern_index += 2;
+            } else {
+                pattern_index = 0;
+            }
+        } else if remaining_pattern.starts_with("[^") {
+            if let Some(end) = remaining_pattern.find(']') {
+                let negative_group = &remaining_pattern[pattern_index + 2..end];
+                if !negative_group.contains(c) {
+                    pattern_index += end + 1;
+                } else {
+                    pattern_index = 0;
+                }
+            } else {
+                pattern_index = 0;
+            }
+        } else if remaining_pattern.starts_with("[") {
+            if let Some(end) = remaining_pattern.find(']') {
+                let positive_group = &remaining_pattern[pattern_index + 1..end];
+                if positive_group.contains(c) {
+                    pattern_index += end + 1;
+                } else {
+                    pattern_index = 0;
+                }
+            }
+        } else {
+            if pattern[pattern_index..=pattern_index].contains(c) {
+                pattern_index += 1;
+            } else {
+                pattern_index = 0;
+            }
+        }
     }
+    pattern_index >= pattern.len()
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
